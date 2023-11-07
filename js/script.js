54b1ee238c55221f40b706b21758d3d094a5e897
BriacGL <briac.gauthierlefeuvre@gmail.com>
2023-11-07 15:02:47 +0100

    more values in the script file

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ var secondValue = 2;
 console.log("firstValue :", firstValue);
 console.log("operator :", operator);
 console.log("secondValue :", secondValue);
+
+firstValue = prompt("Entrez la première valeur :");
+secondValue = prompt("Entrez la deuxième valeur :");
+
+console.log("Nouvelle valeur de firstValue :", firstValue);
+console.log("Nouvelle valeur de secondValue :", secondValue);
+
+firstValue = parseFloat(firstValue);
+secondValue = parseFloat(secondValue);
+
+console.log("firstValue + secondValue :", firstValue + secondValue);
